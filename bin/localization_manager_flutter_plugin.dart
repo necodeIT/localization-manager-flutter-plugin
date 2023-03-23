16a647504083c1cf44a80316d935e2122e60e542
officialbishowb <officialbishowb@gmail.com>
2023-03-23 15:34:40 +0100

    Initial commit

---
+++ bin/localization_manager_flutter_plugin.dart
@@ -0,0 +1,6 @@
+import 'package:localization_manager_flutter_plugin/localization_manager_flutter_plugin.dart'
+    as localization_manager_flutter_plugin;
+
+void main(List<String> arguments) {
+  print('Hello world: ${localization_manager_flutter_plugin.calculate()}!');
+}
---
+++ test/localization_manager_flutter_plugin_test.dart
@@ -0,0 +1,8 @@
+import 'package:localization_manager_flutter_plugin/localization_manager_flutter_plugin.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
